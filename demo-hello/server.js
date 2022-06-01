b34482899317d1082c20ef98740596163ba57ae7
Hoang Van Chien <chienhv.ous@mcredit.com.vn>
2022-06-01 10:34:19 +0700

    update

--- demo-hello/server.js
+++ demo-hello/server.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+
 var router1 = require("./apiRouter")
 var router2 = express.Router()
 var router3 = require("./middleware")
